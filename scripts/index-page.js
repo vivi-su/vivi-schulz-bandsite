035bcefcc24ce5fc56a777655978c54128dcbfd0
Vivi Schulz <haweirh@gmail.com>
2022-10-22 15:20:24 -0700

    Add input and textarea fous and blur eventlistner.

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,25 +1,18 @@
-console.log("hello");
+const nameInput = document.getElementById('userName');
+const messageTextArea = document.getElementById('textArea');
 
-const userInput = [
-  {
-    name: "Connor Walton",
-    date: "02/17/2021",
-    content:
-      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
-  },
-  {
-    name: "Emilie Beach",
-    date: "01/09/2021",
-    content:
-      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
-  },
-  {
-    name: "Miles Acosta",
-    date: "12/20/2020",
-    content:
-      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
-  },
-];
+const focusHandler = (event) => {
+  event.target.className = "form--highlight";
+};
+const blurHandler = (event) => {
+  event.target.className = "";
+};
+
+nameInput.addEventListener('focus', focusHandler);
+nameInput.addEventListener('blur', blurHandler);
+
+messageTextArea.addEventListener("focus", focusHandler);
+messageTextArea.addEventListener("blur", blurHandler);
 
 
 const renderInput = (userInput,commentMessage) =>{
@@ -95,7 +88,6 @@ form.addEventListener('submit', (event)=>{
       content: commentVal,
     });
     displayComment();
-    console.log(commentDate);
     event.target.reset();
   }else{
     alert('Please type both Inputs');
