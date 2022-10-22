caba28c5a0f9d683bbfdbf294b3d70fa5e0b9041
Vivi Schulz <haweirh@gmail.com>
2022-10-22 15:20:24 -0700

    Add input and textarea fous and blur eventlistner.

---
+++ scripts/index-userInput.js
@@ -0,0 +1,20 @@
+const userInput = [
+  {
+    name: "Connor Walton",
+    date: "02/17/2021",
+    content:
+      "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains.",
+  },
+  {
+    name: "Emilie Beach",
+    date: "01/09/2021",
+    content:
+      "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day.",
+  },
+  {
+    name: "Miles Acosta",
+    date: "12/20/2020",
+    content:
+      "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough.",
+  },
+];
