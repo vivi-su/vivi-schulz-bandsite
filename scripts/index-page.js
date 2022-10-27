a429aea8f3ca3dd7a00712f6892decfa3ce9bdd1
Vivi Schulz <haweirh@gmail.com>
2022-10-27 10:56:26 -0700

    Fix newest comment to top.

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -25,19 +25,11 @@ messageTextArea.addEventListener("focus", focusHandler);
 messageTextArea.addEventListener("blur", blurHandler);
 
 
-
-const generateContent = (data) => {
-
-};
-
-
 //render-------------------
 const render = (data) => {
  
   const comment = document.querySelector(".comment");
   comment.innerHTML = " ";
-
-  generateContent(data);
 
   data.forEach((key) => {
     const commentSection = document.createElement("article");
@@ -78,71 +70,18 @@ const render = (data) => {
     const createHr = document.createElement("hr");
     createHr.classList.add("form__hr");
     comment.appendChild(createHr);
-    console.log(`call me babay1`);
-  });
-};
-
-//render-------------------
-const render2 = (data) => {
-
-  const comment = document.querySelector(".comment");
-  comment.innerHTML = " ";
-
- let arr=[];
-  arr.unshift(data);
-  console.log(arr);
-  arr += arr.forEach(() => {
-    const commentSection = document.createElement("article");
-    commentSection.classList.add("comment__section");
-    comment.appendChild(commentSection);
-
-    const commentMessageWraper = document.createElement("div");
-    commentMessageWraper.classList.add("comment__message-wraper");
-    commentSection.appendChild(commentMessageWraper);
-
-    const commentMessageImage = document.createElement("div");
-    commentMessageImage.classList.add("comment__message-image");
-    commentMessageWraper.appendChild(commentMessageImage);
-
-    const commentMessage = document.createElement("section");
-    commentMessage.classList.add("comment__message");
-    commentSection.appendChild(commentMessage);
-
-    const commentMessageU = document.createElement("p");
-    commentMessageU.classList.add("comment__message-user");
-    commentMessageU.innerText = data.name;
-
-    const commentMessageD = document.createElement("p");
-    commentMessageD.classList.add("comment__message-date");
-     const today = new Date();
-
-     commentMessageD.innerText = getDate(today);
-
-    const commentMessageC = document.createElement("p");
-    commentMessageC.classList.add("comment__message-content");
-    commentMessageC.innerText = data.comment;
-
-    commentMessage.appendChild(commentMessageU);
-    commentMessage.appendChild(commentMessageD);
-    commentMessage.appendChild(commentMessageC);
-
-    const createHr = document.createElement("hr");
-    createHr.classList.add("form__hr");
-    comment.appendChild(createHr);
-    console.log(`call me babay2`);
   });
 };
 
 
 
-//form event listener
+//form event listener---------------------------------------------------
 const form = document.querySelector(".form__form");
 const checkFormClicked = form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nameVal = event.target.userName.value;
   const commentVal = event.target.textArea.value;
-  // const commentDate = getDate();
   const postUrl = "https://project-1-api.herokuapp.com/comments?api_key={{SPRINT3__KEY}}";
 
   const commentInput = {
@@ -156,15 +95,10 @@ const checkFormClicked = form.addEventListener("submit", (event) => {
   axios
       .post(postUrl, commentInput)
       .then((response) => {
-        console.log("data2 origin", response);
-        return response.data;
+        commentArray.unshift(response.data);
+        render(commentArray);   
+        form.reset();   
       })
-
-      .then((response) => {
-        render2(response);
-        return response;
-      })
-
       .catch((err) => {
         console.log(err);
       });
@@ -176,24 +110,18 @@ const checkFormClicked = form.addEventListener("submit", (event) => {
 
 });
 
-//Postman Get
-const call = ()=>{
+//----------------------------Postman Get----------------------------
+let commentArray = [];
 
-  const defaultUrl =
-    "https://project-1-api.herokuapp.com/comments?api_key={{SPRINT3__KEY}}";
-axios.get(defaultUrl)
-     .then(response =>response.data)
-     .then(render)
-     .catch((err) => {
-         console.log(err.err);
+  const getUrl = axios.get(
+    "https://project-1-api.herokuapp.com/comments?api_key={{SPRINT3__KEY}}"
+  );
+
+getUrl
+  .then((response) => {
+    commentArray = response.data;
+    render(commentArray);
   })
-}
-
-let buttonClicked = true;
-
-if (buttonClicked){
-call();
-}else{
-form.reset();
-checkFormClicked();
-}
+  .catch((err) => {
+    console.log(err.err);
+  });
